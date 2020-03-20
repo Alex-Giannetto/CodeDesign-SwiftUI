7cfefca56f423853b199fc134ad40d40400be47a
Alexg78bis <alexg78bis@gmail.com>
2020-03-20 22:08:18 +0100

    Initial Commit

---
+++ Design+Code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Design+Code
+//
+//  Created by Alex on 20/03/2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
