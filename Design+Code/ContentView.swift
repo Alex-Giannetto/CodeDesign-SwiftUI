ece2f15ac6ead66c09b32b70089a499f6baf1b70
Alexg78bis <alexg78bis@gmail.com>
2020-03-21 15:34:19 +0100

    video 4 : State animation

--- Design+Code/ContentView.swift
+++ Design+Code/ContentView.swift
@@ -9,6 +9,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    
+    var body: some View {
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+            
+            BackCardView()
+                .background(show ? Color("card3") : Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle.degrees(show ? 0 : 10))
+                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+            
+            BackCardView()
+                .background(show ? Color("card4") : Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .scaleEffect(0.95)
+                .rotationEffect(Angle.degrees(show ? 0 : 5))
+                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
+            
+            CardView()
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+            }
+            
+            BottomCardView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct CardView: View {
     var body: some View {
         VStack {
             HStack {
@@ -38,8 +89,55 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340.0, height: 220)
+    }
+}
+
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificate")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            
+            Image("Background1")
+            
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            
+            Text("This certificate is proof that Alexg78bis has achieved the UI Design course with approval from a Design+Code instructor")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+        .offset(x:0, y:500)
     }
 }
