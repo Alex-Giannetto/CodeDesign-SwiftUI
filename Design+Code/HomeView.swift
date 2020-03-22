626bdc962cdc2ebfaab7b67d7b9d83f55d56c1d4
Alexg78bis <alexg78bis@gmail.com>
2020-03-22 12:30:17 +0100

    video 13 : section card

--- Design+Code/HomeView.swift
+++ Design+Code/HomeView.swift
@@ -26,6 +26,16 @@ struct HomeView: View {
             .padding(.horizontal)
             .padding(.top, 30)
             
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(0 ..< 5) { item in
+                        SectionView()
+                    }
+                }
+                .padding(.horizontal, 14)
+                .padding(.bottom, 60)
+            }
+            
             Spacer()
         }
     }
@@ -34,5 +44,37 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype design in SwiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Image("Logo1")
+            }
+            
+            Text("18 Sections".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+            
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.5), radius: 20, x: 0, y: 20)
     }
 }
